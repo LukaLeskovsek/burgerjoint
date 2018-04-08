5f06b7d2fb958a26e7d14fa5867bb48ebb2efb56
Luka Leskovsek <luka@leskovsek.si>
2018-04-08 21:52:20 +0200

    Toolbar and Logo

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Aux from "../../../hoc/Auxx";
+import Button from '../../UI/Button/Button';
 
 const orderSummary = props => {
   const ingredientSummary = Object.keys(props.ingredients).map(igKey => {
@@ -16,7 +17,10 @@ const orderSummary = props => {
       <h3>Order summary</h3>
       <p>You've selected the following ingredients</p>
       <ul>{ingredientSummary}</ul>
+      <p><strong>Total price : {props.price.toFixed(2)}</strong></p>
       <p>Continue to Checkout?</p>
+      <Button btnType='Danger' clicked={props.purchaseCancelled}>CANCEL</Button>
+      <Button btnType="Success" clicked={props.purchaseContinued}>CONTINUE</Button>
     </Aux>
   );
 };
